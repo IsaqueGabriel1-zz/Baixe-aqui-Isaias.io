fd573d801c6d7829481b827b26734fb7004852f3
IsaqueGabriel1 <isaquegabriel2002@outlook.com>
2022-05-29 13:17:30 -0300

    Baixe aqui Isaias

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const ytdl = require("ytdl-core");
+const port = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.get("/query", (req, res) => {
+    let url = req.query["teste"]
+    if(url){
+        res.header("Content-Disposition", '"attachmentt; filename="video.mp4"')
+        ytdl(url).pipe(res);
+    }else{
+        res.send("Nenhuma url foi passada")
+    }
+})
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+
+app.listen((port), () => {console.log("Tudo certo")})
